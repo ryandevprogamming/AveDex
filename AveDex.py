a5e29134b10ce68fc06c8479abad4bd81068e898
Ryan Dos Santos Leal <ryanparatodosaplicativos@gmail.com>
2026-08-04 15:49:13 -0300

    Refatora comparação usando campos configuráveis

--- AveDex.py
+++ AveDex.py
@@ -223,51 +223,26 @@ def imprimir_linha_comparacao(rotulo, valor_1, valor_2):
 
 
 def exibir_comparacao_aves(ave_1, ave_2):
-    # Exibe os dados das duas aves lado a lado.
+    # Cabeçalho da comparação.
     print()
-    print("=" * 78)
+    print(linha("=", 78))
     print("COMPARAÇÃO ENTRE AVES")
-    print("=" * 78)
+    print(linha("=", 78))
 
+    # Primeira linha com o nome das aves.
     imprimir_linha_comparacao(
         "Campo", ave_1["nome_popular"], ave_2["nome_popular"]
     )
-    print("-" * 78)
-    imprimir_linha_comparacao(
-        "Nome científico",
-        ave_1.get("nome_cientifico"),
-        ave_2.get("nome_cientifico"),
-    )
-    imprimir_linha_comparacao("Ordem", ave_1.get("ordem"), ave_2.get("ordem"))
-    imprimir_linha_comparacao(
-        "Família", ave_1.get("familia"), ave_2.get("familia")
-    )
-    imprimir_linha_comparacao(
-        "Dieta", ave_1.get("dieta_tipo"), ave_2.get("dieta_tipo")
-    )
-    imprimir_linha_comparacao(
-        "Habitat", ave_1.get("habitat"), ave_2.get("habitat")
-    )
-    imprimir_linha_comparacao(
-        "Comprimento",
-        valor_ou_indisponivel(ave_1.get("comprimento_cm"), "cm"),
-        valor_ou_indisponivel(ave_2.get("comprimento_cm"), "cm"),
-    )
-    imprimir_linha_comparacao(
-        "Peso",
-        valor_ou_indisponivel(ave_1.get("peso_g"), "g"),
-        valor_ou_indisponivel(ave_2.get("peso_g"), "g"),
-    )
-    imprimir_linha_comparacao(
-        "Conservação",
-        ave_1.get("status_conservacao", "Não informado"),
-        ave_2.get("status_conservacao", "Não informado"),
-    )
-    imprimir_linha_comparacao(
-        "Índice",
-        ave_1.get("indice_conservacao", "Não informado"),
-        ave_2.get("indice_conservacao", "Não informado"),
-    )
+    print(linha("-", 78))
+
+    # Percorre todos os campos configurados em CAMPOS_COMPARACAO.
+    for rotulo, campo, unidade in CAMPOS_COMPARACAO:
+        # Obtém e formata o valor da primeira ave.
+        valor_1 = valor_ou_indisponivel(ave_1.get(campo), unidade)
+        # Obtém e formata o valor da segunda ave.
+        valor_2 = valor_ou_indisponivel(ave_2.get(campo), unidade)
+        # Imprime a linha já formatada.
+        imprimir_linha_comparacao(rotulo, valor_1, valor_2)
 
 
 def escolher_ave(catalogo, mensagem):
